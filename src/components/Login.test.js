25f59b5b50133acb2ae1279938710e5738d8f2f5
pierrek6e <pierrecass75@gmail.com>
2022-05-18 12:56:38 +0700

    input fields should be empty

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -4,19 +4,31 @@ import Login from "./Login";
 describe("Login component", () => {
   test("email input should be rendered", () => {
     render(<Login />);
-    const emailInputElement = screen.getByPlaceholderText(/email/i)
+    const emailInputElement = screen.getByPlaceholderText(/email/i);
     expect(emailInputElement).toBeInTheDocument();
   });
 
   test("password input should be rendered", () => {
     render(<Login />);
-    const passwordInputElement = screen.getByPlaceholderText(/password/i)
+    const passwordInputElement = screen.getByPlaceholderText(/password/i);
     expect(passwordInputElement).toBeInTheDocument();
   });
 
   test("login button should be rendered", () => {
     render(<Login />);
-    const loginButtonElement = screen.getByRole("button")
+    const loginButtonElement = screen.getByRole("button");
     expect(loginButtonElement).toBeInTheDocument();
+  });
+
+  test("email input should be empty", () => {
+    render(<Login />);
+    const emailInputElement = screen.getByPlaceholderText(/email/i);
+    expect(emailInputElement.value).toBe("");
+  });
+
+  test("password input should be empty", () => {
+    render(<Login />);
+    const passwordInputElement = screen.getByPlaceholderText(/password/i);
+    expect(passwordInputElement.value).toBe("");
   });
 });
